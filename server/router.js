380785ba21dd10d245d1ea360500170a6fa454b4
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-24 09:37:30 -0700

    Added router for register, writepost,login, yelp api

--- server/router.js
+++ server/router.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 
-module.exports = router;
+router
+    .post('/register', controller.register)
+    .post('/writepost',controller.writepost)
+    .get('/login', controller.login)
+    .get('/yelp', controller.restaurant)
+
+    module.exports = router;
