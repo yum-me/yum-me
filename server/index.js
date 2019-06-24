bcef0dc9eab44fd9fc73c7d8eb6801dc5693e30f
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-24 09:05:59 -0700

    Added writepost, login, register, yelpapi

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
 const router = require('./router');
+const db = require('../database/index.js')
 
 const app = express();
 const port = process.env.PORT || 3000;
--- server/router.js
+++ server/router.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router
+    .post('/register', controller.register)
+    .post('/writepost',controller.writepost)
+    .get('/login', controller.login)
+    .get('/yelp', controller.restaurant)
+    
 
 module.exports = router;
