bc298d31efaa697071ff758479b4d81e8ad16295
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-06-24 09:41:11 -0700

    Add 'browse' and 'search' routes

--- server/router.js
+++ server/router.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router.route('/browse')
+  .get(controller.findRecent);
+
+router.route('/search')
+  .get(controller.search);
+
 
 module.exports = router;
