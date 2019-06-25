e0df73a15430778a33df13a61e687bbcae6c73e7
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-25 14:51:39 -0700

    able to send ajax request to server to login

---
+++ client/src/Components/User/Login.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import User from './User.css'
+import axios from 'axios';
+import Register from './Register.jsx'
+import Home from '../Home/Home.jsx'
+import { Route, Link, BrowserRouter as Router } from "react-router-dom"
+
+
+class Login extends React.Component {
+  constructor() {
+    super()
+    this.state ={ 
+      email: "",
+      password: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  submit(e){
+    e.preventDefault();
+    const { email, password } = this.state;
+    axios
+        .get('/login', {params: {email, password}})
+        .then((data) => {
+          <Route exact path="/home" component={Home} />
+          ReactDOM.render(<Home />, document.getElementById("app"));
+          console.log('Succesfully Login')
+        })
+        .catch(err => console.log("Error login", err))
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="userRegister" onSubmit={this.submit} ref={form => this.form = form}>
+          <div>
+            <img src="" ></img>
+            <h1>Login</h1>
+          </div>
+          <input className="usernameRegister input" type="email" name="email" placeholder="E-mail" onChange={this.handleChange} required/>
+          <input className="usernameRegister input" type="password" minlength="6" name="password" placeholder="Password"onChange={this.handleChange} required />
+          <div>
+            <a className="submitRegister"  href="#">
+            <input className="submitRegister input" type="submit" value="Sign Up" />
+            </a>
+            
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+export default Login
