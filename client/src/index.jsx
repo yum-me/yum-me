4dc278a7f0eaec58173bcda5d7a7b1fdee25827c
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-24 17:46:44 -0700

    update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Home from './Components/Home/Home.jsx';
 import Browse from './Components/Browse/Browse.jsx';
+import CreatePost from './Components/CreatePost/CreatePost.jsx'
 
-ReactDOM.render(<Browse />, document.getElementById('app'));
+ReactDOM.render(<CreatePost />, document.getElementById('app'));
