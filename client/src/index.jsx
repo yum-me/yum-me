19f408dfe17fb21a9baed9defb2e2db8a5b3c5aa
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-06-25 16:52:29 -0700

    Integrate Ufuk's index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,9 @@ import Navbar from './Components/Navbar/Navbar.jsx';
 import Home from './Components/Home/Home.jsx';
 import Browse from './Components/Browse/Browse.jsx';
 import Search from './Components/Search/Search.jsx';
-import CreatePost from './Components/CreatePost/CreatePost.jsx'
+import CreatePost from './Components/CreatePost/CreatePost.jsx';
+import Register from './Components/User/Register.jsx';
+import Login from './Components/User/Login.jsx';
+
 
 ReactDOM.render(<Home />, document.getElementById('app'));
