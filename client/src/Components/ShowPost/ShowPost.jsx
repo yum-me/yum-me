da5c3f3fda404d7ef8c09532746dabfb96a90470
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-06-25 19:24:07 -0700

    Add styling to show post

--- client/src/Components/ShowPost/ShowPost.jsx
+++ client/src/Components/ShowPost/ShowPost.jsx
@@ -25,7 +25,7 @@ class ShowPost extends React.Component {
   }
 
   fetchOnePost () {
-    axios.get('http://localhost:3000/post', {params: {_id: '5d10404f189fe79e615a8886'}})
+    axios.get('/post', {params: {_id: '5d0eb62076ee652e557d864c'}})
     .then(({data}) => this.setState({post: data[0]}))
     .then(() => {
       return this.state.post.comments.sort(function(a, b) {
@@ -47,7 +47,7 @@ class ShowPost extends React.Component {
   handleSubmit (event) {
     const { text } = this.state;
     event.preventDefault()
-    axios.post('http://localhost:3000/comment', {text}, {params: {_id: '5d10404f189fe79e615a8886'}})
+    axios.post('/comment', {text}, {params: {_id: '5d0eb62076ee652e557d864c'}})
     .then(() => this.fetchOnePost())
     .catch(() => console.error('Error with adding comment'))
     
@@ -55,7 +55,7 @@ class ShowPost extends React.Component {
   
   handleLikePost () {
     this.setState({like: true})
-    axios.post('http://localhost:3000/post', {params: {_id: '5d10404f189fe79e615a8886'}})
+    axios.post('/post', {params: {_id: '5d0eb62076ee652e557d864c'}})
     .then(() => this.fetchOnePost())
     .catch(() => console.error('Error with adding comment'))
   }
@@ -63,30 +63,45 @@ class ShowPost extends React.Component {
   render () {
     const { author, createdAt, image, likes, recommend, restaurant, text, title } = this.state.post;
     const { comments } = this.state;
+    const postAuthor = author ? author.username : null;
+    const recommendImage = recommend === "Yes" ? 
+      <img className="post-recommend-img" src="https://res.cloudinary.com/kjhogan/image/upload/v1536097829/happy_dbmo3c.png"></img> :
+      <img className="post-recommend-img" src="https://res.cloudinary.com/kjhogan/image/upload/v1536097829/sad_fcfqhu.png"></img>
+    
     return (
       <div> 
         <NavBar />
-        <div className="show-post-main">
-          <div className="show-post-user">
-            <img className="show-post-avatar" src={author ? author.avatar : ''} />
-            <p>{author ? author.username : ''}</p>
-          </div>
-          <p>Restaurant: {restaurant}</p>
-          <img className="show-post-image" src={image} />
-          <div className="show-post-content">
-            <p>{title}</p>
-            <p>{text}</p>  
-          </div>
-          <p>{moment(createdAt).fromNow()}</p>
-          <hr / >
-          <span>{comments ? comments.length : ''} comments</span>
-          <button onClick={this.handleLikePost} disabled={this.state.like}> {likes} likes</button>
-          <form>
-            <textarea className="comment-input" name="text" placeholder="Write a comment..." onChange={this.handleChange}/>
-            <button type="submit" onClick={this.handleSubmit}>Add Comment</button>
-          </form>
-          <div className="show-post-comments">
-            {comments ? comments.map((item, index) => <PostComment item={item} key={index} />): ''}
+        <div className="show-post-container">
+          <div className="show-post-main">
+            <div className="show-post-post-container">
+              <img className="show-post-image" src={image} />
+              <div className="show-post-content">
+                <div className="show-post-title">
+                  <h1>{title}</h1>
+                  <div>{recommendImage}</div>                 
+                </div>
+                <div className="show-post-user">
+                  <img className="show-post-avatar" src={author ? author.avatar : ''} />
+                  <div>
+                    <h3>{postAuthor}</h3>
+                    <p>{moment(createdAt).fromNow()}</p>
+                  </div>
+                </div>
+                <p className="show-post-text">{text}</p>  
+              </div>
+              <p>Restaurant: {restaurant}</p>
+            </div>
+            <div className="show-post-comments-container">
+              <span>{comments ? comments.length : ''} comments</span>
+              <button onClick={this.handleLikePost} disabled={this.state.like}> {likes} likes</button>
+              <form>
+                <textarea className="comment-input" name="text" placeholder="Write a comment..." onChange={this.handleChange}/>
+                <button type="submit" onClick={this.handleSubmit}>Add Comment</button>
+              </form>
+              <div className="show-post-comments">
+                {comments ? comments.map((item, index) => <PostComment item={item} key={index} />): ''}
+              </div>
+            </div>
           </div>
         </div>
       </div>
