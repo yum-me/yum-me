91eeb3e185cac76591276976b4ae639c260ef8fb
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-25 19:35:29 -0700

    Render different component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,4 +9,4 @@ import Register from './Components/User/Register.jsx'
 import Login from './Components/User/Login.jsx'
 
 
-ReactDOM.render(<Home />, document.getElementById('app')
+ReactDOM.render(<Register />, document.getElementById('app'));
